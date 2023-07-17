892ff559d07a32e929b82ec85d4042fbfff25eb2
Dragos Neacsu <dragos.neacsu@yopeso.com>
2023-07-17 12:01:34 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dragos Neacsu on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
